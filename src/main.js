7e849f4358d85c3a4b24b6794ef05b8eba9c9149
Arionel <arionel0794@gmail.com>
2026-09-19 05:45:59 +0300

    first

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import { setupCounter } from './counter.js'
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+<main class="flex justify-center flex-col max-w-[1080px] m-0 mx-auto text-center" >
+<h1 class="text-2xl text-amber-200" >Kanban</h1>
+      <button
+        class="w-[120px] mx-auto mt-4 px-4 py-2 bg-blue-500 text-white rounded-sm hover:bg-blue-600"
+      >
+        Get Started
+      </button>
+</main>
+`
+
+setupCounter(document.querySelector('#counter'))
